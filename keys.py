26e6e906d0e0abd8657b8b06a68e3d6ab25dc74a
CJ Campbell <cjcampbell844@gmail.com>
2022-06-13 09:56:10 -0400

    Initial commit

---
+++ keys.py
@@ -0,0 +1,3 @@
+speech_to_text_key = 'dmZ9U_6DQlBqRq7O-IirxE4kqaJKKiYfyG4sW73qzzVW'
+text_to_speech_key = '2PjjX0TFbLhPRnu1JiOA9tkV4vniG_aZXSmAcMuWjdyo'
+translate_key = 'XlL-0YMW8CtRl9Ga9HeulhOeJ1pDt-TXM9oF4aQvdnOz'
